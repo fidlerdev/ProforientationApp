2da3a91e4fdc2b81178cd9d8e607f04855c50274
Benjamin Fidler <fidlerdev@yandex.ru>
2020-05-04 08:05:26 +0300

    Updates

--- precache-manifest.90c4506ec738421d4e7173f33bc021ad.js
+++ precache-manifest.90c4506ec738421d4e7173f33bc021ad.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "82a78580b2c64454139b47d57a1308d0",
+    "revision": "8cd28c3df25a1f40fb736b3cab4213c1",
     "url": "/my-school-project/index.html"
   },
   {
-    "revision": "6220054b3b6a61f2f24c",
+    "revision": "23826659f41462327fe0",
     "url": "/my-school-project/static/css/2.551d3f2e.chunk.css"
   },
   {
-    "revision": "92ff2cb57a4400d079eb",
+    "revision": "9a4181b6f6ced4811290",
     "url": "/my-school-project/static/css/main.00f198ce.chunk.css"
   },
   {
-    "revision": "6220054b3b6a61f2f24c",
-    "url": "/my-school-project/static/js/2.5be700f6.chunk.js"
+    "revision": "23826659f41462327fe0",
+    "url": "/my-school-project/static/js/2.3ef2b6bd.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/my-school-project/static/js/2.5be700f6.chunk.js.LICENSE.txt"
+    "url": "/my-school-project/static/js/2.3ef2b6bd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "92ff2cb57a4400d079eb",
-    "url": "/my-school-project/static/js/main.02e62f3d.chunk.js"
+    "revision": "9a4181b6f6ced4811290",
+    "url": "/my-school-project/static/js/main.7f18ac2e.chunk.js"
   },
   {
     "revision": "4fdad0f34cff08d57081",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-school-project/precache-manifest.576615c551002a82fa2363213fba69ef.js"
+  "/my-school-project/precache-manifest.90c4506ec738421d4e7173f33bc021ad.js"
 );
 
 self.addEventListener('message', (event) => {
