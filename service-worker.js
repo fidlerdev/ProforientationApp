dc2057c43e05f677734bc05a5c6c74ecd6db5dca
Benjamin Fidler <fidlerdev@yandex.ru>
2020-05-11 20:20:06 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-school-project/precache-manifest.1d28a66a0c915562695039272c8b9eab.js"
+  "/my-school-project/precache-manifest.9eab94735833ffdd2d9ddd8a59ccde38.js"
 );
 
 self.addEventListener('message', (event) => {
